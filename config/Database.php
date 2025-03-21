87da81a6fbfa32d83284e8db19872a333fce6155
Chelsey Hecker <cnhecker@mail.fhsu.edu>
2025-03-20 21:16:06 -0400

    Restoring port

--- config/Database.php
+++ config/Database.php
@@ -6,18 +6,20 @@
     private $username;
     private $password;
     private $conn;
+    private $port;
 
     public function __construct(){
       $this->username = getenv('USERNAME');
       $this->password = getenv('PASSWORD');
       $this->db_name = getenv('DBNAME');
       $this->host = getenv('HOST');
+      $this->port = getenv('PORT');
     }
 
     // DB Connect
     public function connect() {
       $this->conn = null;
-      $dsn = "pgsql:host={$this->host};dbname={$this->db_name};";
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};";
       try { 
         $this->conn = new PDO($dsn, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
