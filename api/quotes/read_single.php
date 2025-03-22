8f07b27afc2a67325b7dbf1f4253ac04016e467b
Chelsey Hecker <cnhecker@mail.fhsu.edu>
2025-03-21 21:27:47 -0400

    fixed files imports on quotes read_single

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -4,8 +4,8 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 // Include database and model
-include_once '../config/Database.php';
-include_once '../models/Quote.php';
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
 // Instantiate DB & connect
 $database = new Database();
